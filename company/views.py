aaa35b0773164c65fc568b3abcc26363ba0eb3bb
Shar221 <sharlinenm2020@gmail.com>
2025-10-28 10:21:54 +0300

    allow viewing and creation of companies in views.py

--- company/views.py
+++ company/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Company
+from .serializers import CompanySerializer
 
-# Create your views here.
+class CompanyListCreateView(generics.ListCreateAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
