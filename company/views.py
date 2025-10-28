d7d39ece24b1145be140ea9d4c3ab83c4befb5ca
Shar221 <sharlinenm2020@gmail.com>
2025-10-28 10:37:18 +0300

    modify serializers

--- company/views.py
+++ company/views.py
@@ -6,7 +6,7 @@ from .serializers import CompanySerializer
 class CompanyListCreateView(generics.ListCreateAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
