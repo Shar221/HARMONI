8a84e89b0c81202a1e5a3605ac9f594bceac0e1e
Shar221 <sharlinenm2020@gmail.com>
2025-11-09 20:06:37 -0500

    CompanyEmployeesView

--- company/urls.py
+++ company/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CompanyCreateView, GetCompanyView
+from .views import CompanyCreateView, GetCompanyView, GetAllCompaniesView , CompanyEmployeesView
 
 urlpatterns = [
     path('create/', CompanyCreateView.as_view(), name='create-company'),
     path('get/', GetCompanyView.as_view(), name='get-company'),
+    path('all/', GetAllCompaniesView.as_view(), name='get-all-companies'),
+    path('<int:company_id>/employees/', CompanyEmployeesView.as_view(), name='company-employees'),
 ]
--- company/views.py
+++ company/views.py
@@ -2,6 +2,9 @@ from rest_framework import generics, permissions
 from rest_framework.response import Response
 from .models import Company
 from .serializers import CompanySerializer
+from authentication.models import User
+from authentication.serializers import UserSerializer
+from rest_framework.exceptions import PermissionDenied
 
 class CompanyCreateView(generics.CreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -21,3 +24,24 @@ class GetCompanyView(generics.RetrieveAPIView):
             serializer = self.get_serializer(company)
             return Response(serializer.data)
         return Response({"detail": "No company associated with this user."}, status=404)
+    
+class GetAllCompaniesView(generics.ListAPIView):
+    permission_classes = [permissions.IsAdminUser]
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all()
+
+
+
+
+class CompanyEmployeesView(generics.ListAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        company_id = self.kwargs.get('company_id')
+        user = self.request.user
+
+        if user.is_superuser or (hasattr(user, 'company') and user.company.id == int(company_id)):
+            return User.objects.filter(company_id=company_id)
+
+        raise PermissionDenied("You do not have permission to view employees for this company.")
